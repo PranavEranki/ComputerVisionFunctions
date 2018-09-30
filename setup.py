c093ce1a8a5408557fc2515b18fe99fbc4288468
7610 <leletinger@gmail.com>
2018-09-30 03:12:27 -0700

    uploading

--- setup.py
+++ setup.py
@@ -11,8 +11,10 @@ setuptools.setup(
     description="An easy to use, high-level computer vision library containing assorted functions",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
+    url="https://github.com/PranavEranki/ComputerVisionFunctions",
+    packages=['ComputerVisionFunctions'],
+    include_packade_data=True,
+    zip_safe=False,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
